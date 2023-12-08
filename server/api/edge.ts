ffbdc1ef349db9dda33f377d0963536b0328a6a5
Jazee6 <jmzzzcn@gmail.com>
2023-12-08 21:31:02 +0800

    V0.6 支持SDXL 1.0

--- server/api/edge.ts
+++ server/api/edge.ts
@@ -1,7 +1,3 @@
-export const config = {
-    runtime: 'edge',
-};
-
 export default async function handler() {
     const encoder = new TextEncoder();
     const customReadable = new ReadableStream({
