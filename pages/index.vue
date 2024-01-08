584e8e858c5b9609f064618cf9523ac5811ea826
roshanca <wwj1983@gmail.com>
2024-01-08 16:42:03 +0800

    Add clear button for input field

--- pages/index.vue
+++ pages/index.vue
@@ -490,6 +490,11 @@ function handleImageAdd() {
           <UButton @click="handleReq($event, selectedModel)" :disabled="loading" class="m-1">
             发送
           </UButton>
+          <UButton color="gray" size="md" class="m-1" @click="() => input = ''">
+            <template #leading>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24"><path fill="currentColor" d="M8 20v-5h2v5h9v-7H5v7h3zm-4-9h16V8h-6V4h-4v4H4v3zM3 21v-8H2V7a1 1 0 0 1 1-1h5V3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v3h5a1 1 0 0 1 1 1v6h-1v8a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1z" /></svg>
+            </template>
+          </UButton>
         </div>
       </div>
     </div>
