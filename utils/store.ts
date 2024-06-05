c9de3574d2dbaed428a566d765c2f1883ba32877
Jazee6 <jmzzzcn@gmail.com>
2024-06-05 22:47:49 +0800

    fix: image too large

--- utils/store.ts
+++ utils/store.ts
@@ -1,13 +1,11 @@
 import {createGlobalState} from "@vueuse/shared"
-import {textGenModels} from "./db"
-import {ref} from "vue"
 
 export const useGlobalState = createGlobalState(() => {
     const openModelSelect = ref(false)
     const passModal = ref(false)
     const openAside = ref(false)
     const openSettings = ref(false)
-    const selectedModel = ref(textGenModels[0])
+    const selectedModel = ref(uniModals[0])
 
     return {
         openModelSelect,
